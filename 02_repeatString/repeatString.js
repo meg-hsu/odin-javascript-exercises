9f9bf084743eb8fbc032c240df42514747f2b7b7
Meg-Hsu <meghsu@Megs-MBP-2.localdomain>
2024-08-08 15:48:06 -0700

    Wrote function which repeats a string for a specified number of times and passes all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    if (num == 0) return "";
+    if (num > 0){
+        repeated = "";
+        do {
+            repeated = repeated + word;
+            num--;
+        } while (num > 0);
+        return repeated;
+    } 
+    return "ERROR";
 };
 
 // Do not edit below this line
