fc157436e041a041ad3e2275dc7045450bcdae6a
Meg-Hsu <meghsu@Megs-MBP-2.localdomain>
2024-08-08 15:58:50 -0700

    Wrote a function which reverses input

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let reversed = "";
+    for (i = 0; i < input.length; i++){
+        reversed = input.at(i) + reversed;
+    }
+    return reversed;
 };
 
 // Do not edit below this line
