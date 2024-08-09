e3c00bf4f395bbee6e90df255399167fdd686f78
Meg-Hsu <meghsu@Megs-MBP-2.localdomain>
2024-08-08 18:37:17 -0700

    Made function that takes two numbers and calculates the sum of both numbers and every number in between the two of them.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
 const sumAll = function() {
+    nums = []
+    for (i = 0; i < arguments.length; i++){
+        arg = arguments[i];
+        if (!isValid(arg) || i > 2) return 'ERROR';
+        nums.push(arg);
+    }
 
+    nums.sort();
+    currentsum = 0;
+    for (i = nums[0]; i <= nums[1]; i++){
+        currentsum += i;
+    }
+
+    return currentsum;
+};
+
+const isValid = function(input){ // Checks if the input is a number that is 0 or positive and is a whole number
+    return ((typeof input === 'number') && input >= 0 && (input % 1 === 0));
 };
 
 // Do not edit below this line
