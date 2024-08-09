a09ebfc89927eee0b490b254892fc01cfb9de9b2
Meg-Hsu <meghsu@Megs-MBP-2.localdomain>
2024-08-08 18:07:22 -0700

    Created function which takes in an array and any number of additional inputs and removes them from the array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,32 @@
 const removeFromArray = function() {
+    inputArr = arguments[0];
+    removeArr = [];
+    indices = [];
+
+    for (i = 1; i < arguments.length; i++){ // Add all items to be removed to an array
+        removeArr.push(arguments[i]);
+    }
+
+    for (inputIndex = 0; inputIndex < inputArr.length; inputIndex++){
+        found = false; // make index marker
+        for (remIndex = 0; remIndex < removeArr.length; remIndex++){ 
+            if (!found){ 
+                if (inputArr[inputIndex] === removeArr[remIndex]){
+                    indices.push(inputIndex); // Record index of number to be removed
+                    found = true; // Marks number as found
+                }
+            }
+        }
+    }
+    indices.reverse();
+    console.log(indices);
+
+    for (i = 0; i < indices.length; i++){ // for each index
+        inputArr.splice(indices[i], 1);
+    }
+
+    console.log(inputArr);
+    return inputArr;
 };
 
 // Do not edit below this line
