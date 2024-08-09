40b4d57b7013e4842d649e63f26e72550380f383
Meg-Hsu <meghsu@Megs-MBP-2.localdomain>
2024-08-08 18:59:26 -0700

    Wrote functions that convert to and from Celsius and Fahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp = temp - 32;
+  temp = temp * 5/9;
+  return rounding(temp);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp = temp * 9/5;
+  temp = temp + 32;
+  return rounding(temp);
 };
+
+const rounding = function(num){
+  return Math.round(num * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
